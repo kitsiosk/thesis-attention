9e24a0a13e486a9d8821e6ff59744eedd04ab2f0
kitsiosk <kitsiosk@ece.auth.gr>
2019-08-30 19:27:46 +0300

    Functionality fixes

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import numpy as np
 import cv2
 import tkinter
-from imutils.video import VideoStream
+from imutils.video import VideoStream, WebcamVideoStream
 import uuid
 import os
 import yaml
+import time
 
 # Set Variable only for Debugging Processes
 DEBUG = False
-# Define the side of the rectangle that represents the attention point. Side=2*R
+# Define the radius of the circle that represents the attention point
 R = 25
 # Number of frames to save each time the program runs
 NUM_OF_FRAMES = 100
@@ -21,22 +22,16 @@ os.makedirs('dataset/' + UUID + '/positive')
 os.makedirs('dataset/' + UUID + '/negative')
 
 # Enable the camera
-cap = VideoStream(src=0).start()
+cap = cv2.VideoCapture(0)
 """
 Environment-agnostic way to get Screen Dimensions.
-Tkinter is install with almost every python version.
+Tkinter is installed with almost every python version.
 Plus there is no need for external Library and I/O.
 """
 root = tkinter.Tk()
 root.withdraw()
 WIDTH, HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
 if DEBUG: print("Width is {} and Height is {}".format(WIDTH, HEIGHT))
-
-laptop_message = "Which laptop are you using? Press\
-    C for Costas\
-    K for Kitsios\
-    U for Unknown"
-laptop = input(laptop_message)
 
 # Personal data colelction and validation
 print('Hello, we would like to collect data about your gender and age.')
@@ -49,6 +44,13 @@ while True:
     if age.isdigit():
         age = int(age)
         break
+while True:
+    laptop_message = "Which laptop are you using? Press\
+ C for Costas K for Kitsios U for Unknown"
+    laptop = input(laptop_message)
+    if laptop == "k" or laptop == "c" or laptop == "u" or \
+        laptop == "K" or laptop == "C" or laptop == "U":
+        break
 
 data = dict(
     gender=gender,
@@ -58,13 +60,27 @@ data = dict(
     height=HEIGHT,
     laptop=laptop
 )
+
 with open('dataset/' + UUID + '/data.yml', 'w') as outfile:
     yaml.dump(data, outfile)
 
+print("Testing camera stream. Press \'q\' when camera is set")
+# Test camera stream
+while(True):
+    # Capture frame-by-frame
+    _, frame = cap.read()
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+
+    # Press 'q' to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
+
 display_message = (
     "You are about to see {} consequtive screens. For each screen there are two options: If the screen \
-contains a black dot look at the middle of it and press L(right hand). Else,look outside of the screen \
-and press N.(left hand).\
+contains a black dot look at the middle of it and press space. Otherwise, look outside of the screen \
+and press space. Each time space is hit the screen changes\
 \n\
 \nWhen you are ready to start press Enter!").format(NUM_OF_FRAMES)
 input(display_message)
@@ -74,6 +90,8 @@ input(display_message)
 x, y = 0, 0
 i = 0
 while i < NUM_OF_FRAMES:
+    # Variable determining wether we should show a "looking" screen
+    # or a "not looking" screen. Set to i%2 so the screens show in turn
     not_looking = i%2
 
     # Initialize a white screen
@@ -91,7 +109,7 @@ while i < NUM_OF_FRAMES:
     # Else show the point of attention
     if not_looking:
         bottom_left = ((int)(WIDTH/2) - 250, (int)(HEIGHT/2))
-        cv2.putText(screen, "Look away from the screen and press N", bottom_left, cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), lineType=cv2.LINE_AA)
+        cv2.putText(screen, "Look away from the screen and press space", bottom_left, cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), lineType=cv2.LINE_AA)
     else:
         cv2.circle(screen, (x, y), R, (0, 255, 0), -1)
 
@@ -101,22 +119,26 @@ while i < NUM_OF_FRAMES:
                           cv2.WINDOW_FULLSCREEN)
     cv2.imshow('Window', screen)
 
-    # Wait for answer key: L, N, Q or Z
+    # Wait for answer key: Space, Esc or Z
     ans = cv2.waitKey(0)
 
     # Capture a frame immidiately after key hit
-    frame = cap.read()
+    _, frame = cap.read()
+    time.sleep(0.2)
 
-    if ans == ord('l'):
+    if ans == ord(' '):
+        if not DEBUG:
+            if(not not_looking):
+                cv2.imwrite(
+                    'dataset/' + UUID + '/positive/' + UUID + '_' + str(x) + '_' +
+                    str(y) + '.jpg', frame
+                )
+            else:
+                cv2.imwrite(
+                'dataset/' + UUID + '/negative/' + UUID + '_' + str(x) + '_' +
+                str(y) + '.jpg', frame
+                )
         i += 1
-        cv2.imwrite(
-            'dataset/' + UUID + '/positive/' + UUID + '_' + str(x) + '_' +
-            str(y) + '.jpg', frame)
-    elif ans == ord('n'):
-        i += 1
-        cv2.imwrite(
-            'dataset/' + UUID + '/negative/' + UUID + '_' + str(x) + '_' +
-            str(y) + '.jpg', frame)
     # Delete previous frame
     elif ans == ord('z'):
         prev_negative = 'dataset/' + UUID + '/negative/' + UUID + '_' + str(x_prev) + '_' + str(y_prev) + '.jpg'
@@ -131,10 +153,10 @@ while i < NUM_OF_FRAMES:
     elif ans == 27:
         break
     else:
-        print('You must press either l(looking) or n(not looking)')
+        print('You must press either Space, Esc or Z')
 
 # When everything is done, release the video capture
-cap.stop()
+cap.release()
 cv2.destroyAllWindows()
 
 
