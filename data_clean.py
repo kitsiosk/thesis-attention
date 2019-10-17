4e5c68c65a136b7a7f2228894ced6942b35275be
kitsiosk <kitsiosk@ece.auth.gr>
2019-10-17 16:45:27 +0300

    Project structure improvement

--- data_clean.py
+++ data_clean.py
@@ -3,27 +3,27 @@ import json
 with open('data_cleaned.json') as json_file:
     noisy_data = json.load(json_file)
 
-# file = open('noisy2.txt', 'r')
-# # Images list with blank lines separating participants
-# initial_images = file.readlines()
-# # Images list without blank lines
-# images = []
+file = open('noisy2.txt', 'r')
+# Images list with blank lines separating participants
+initial_images = file.readlines()
+# Images list without blank lines
+images = []
 
-# for image in initial_images:
-#     if image == '\n':
-#         continue
-#     images.append(image)
-# print(len(images))
+for image in initial_images:
+    if image == '\n':
+        continue
+    images.append(image)
+print(len(images))
 
 print(len(noisy_data))
-# for i in range(len(images)):
-#     prefix = images[i].split('/')[1].split('_')[0]
-#     path = prefix + '/' + images[i]
-#     # Remove the new line character
-#     path = path[0:-1]
-#     ret = noisy_data.pop(path, None)
-#     # if ret == None:
-#     #     print(path)
+for i in range(len(images)):
+    prefix = images[i].split('/')[1].split('_')[0]
+    path = prefix + '/' + images[i]
+    # Remove the new line character
+    path = path[0:-1]
+    ret = noisy_data.pop(path, None)
+    # if ret == None:
+    #     print(path)
 
 keys = sorted(noisy_data)
 for key in keys:
